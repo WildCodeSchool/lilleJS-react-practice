a3bc9d114d84032baf55e3abffdc48684783acb8
Fantine <fantine.rudent@hotmail.fr>
2023-05-09 14:50:27 +0200

    exo animal list

---
+++ src/AnimalsList/AnimalCard.jsx
@@ -0,0 +1,18 @@
+const AnimalCard = () => {
+
+    // je veux que lorsque je clique sur le bouton avec l'emoji, une alerte s'affiche avec le écrit :
+    // je suis (animalname) et je suis #(animalid).
+    
+    return ( 
+       <div>
+        <h1>
+
+        </h1>
+        <button>
+
+        </button>
+       </div>
+     );
+}
+ 
+export default AnimalCard;
---
+++ src/AnimalsList/AnimalsList.jsx
@@ -0,0 +1,15 @@
+const AnimalsList = () => {
+
+    const animals = [{id: 1, name:'dog', emoji: '🐶'}, {id: 2, name:'cat', emoji: '😸'}, {id: 3, name:'cow', emoji: '🐮'}, { id: 4, name:'sheep', emoji: '🐑'}, {id: 5, name:'horse', emoji: '🐴'}]
+   
+    // ici nous avons un tableau d'animaux - je souhaite les afficher de manière individuelle dans un composant 
+    // AnimalCard (le fichier existe déjà). Je veux que chaque animal soit dans une card individuelle, 
+    // que le name soit affiché dans un H1, que l'emoji soit dans un bouton en font-size 1.5em.
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default AnimalList;
